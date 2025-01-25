f21cda9235a8594eeff3bb6312a7bb97b0a5552d
rahulprops <rahulhack912@gmail.com>
2025-01-26 00:10:57 +0530

    fetch address controller complate

--- backend/server/controller/address.controller.js
+++ backend/server/controller/address.controller.js
@@ -1,6 +1,6 @@
 import errorHandler from "../middleware/err_logs/errorHandler.js"
 import addressModel from "../model/address.molel.js";
-
+//! create address
 export const createAddress=async (req,res)=>{
     const {userId,address,city,pincode,phone,notes}=req.body;
     if(!userId || !address ||!city || !pincode || !phone || !notes){
@@ -17,6 +17,19 @@ export const createAddress=async (req,res)=>{
         return errorHandler(res,400,"create address failed")
     }
     try{}catch(err){
+        return errorHandler(res,500,`server error ${err.message}`)
+    }
+}
+//! fetch address
+export const fetchAddress=async (req,res)=>{
+    try{
+        const {userId}=req.params;
+        const addresss= await addressModel.find({userId})
+        if(!addresss){
+            return errorHandler(res,404,"address not found")
+        }
+        return errorHandler(res,200,"address get sucessful",addresss)
+    }catch(err){
         return errorHandler(res,500,`server error ${err.message}`)
     }
 }
--- backend/server/routes/address.route.js
+++ backend/server/routes/address.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { createAddress } from '../controller/address.controller.js'
+import { createAddress, fetchAddress } from '../controller/address.controller.js'
 const addressRouter=express.Router()
 addressRouter.post("/create",createAddress)
+addressRouter.get("/get/:userId",fetchAddress)
 export default addressRouter;
