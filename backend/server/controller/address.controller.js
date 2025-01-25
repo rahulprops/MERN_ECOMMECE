a6c8bd95461d37c068326362cf38594551676262
rahulprops <rahulhack912@gmail.com>
2025-01-26 00:27:19 +0530

    address delte controller done

--- backend/server/controller/address.controller.js
+++ backend/server/controller/address.controller.js
@@ -1,52 +1,81 @@
-import errorHandler from "../middleware/err_logs/errorHandler.js"
+import errorHandler from "../middleware/err_logs/errorHandler.js";
 import addressModel from "../model/address.molel.js";
 //! create address
-export const createAddress=async (req,res)=>{
-    const {userId,address,city,pincode,phone,notes}=req.body;
-    if(!userId || !address ||!city || !pincode || !phone || !notes){
-        return errorHandler(res,400,"all feild requied")
-    }
-    // create address
-    const addressNew=new addressModel({
-        userId,address,city,pincode,phone,notes
-    })
-    if(addressNew){
-        await addressNew.save()
-        return errorHandler(res,201,"address create sucessful",addressNew)
-    }else{
-        return errorHandler(res,400,"create address failed")
-    }
-    try{}catch(err){
-        return errorHandler(res,500,`server error ${err.message}`)
-    }
-}
+export const createAddress = async (req, res) => {
+  const { userId, address, city, pincode, phone, notes } = req.body;
+  if (!userId || !address || !city || !pincode || !phone || !notes) {
+    return errorHandler(res, 400, "all feild requied");
+  }
+  // create address
+  const addressNew = new addressModel({
+    userId,
+    address,
+    city,
+    pincode,
+    phone,
+    notes,
+  });
+  if (addressNew) {
+    await addressNew.save();
+    return errorHandler(res, 201, "address create sucessful", addressNew);
+  } else {
+    return errorHandler(res, 400, "create address failed");
+  }
+  try {
+  } catch (err) {
+    return errorHandler(res, 500, `server error ${err.message}`);
+  }
+};
 //! fetch address
-export const fetchAddress=async (req,res)=>{
-    try{
-        const {userId}=req.params;
-        const addresss= await addressModel.find({userId})
-        if(!addresss){
-            return errorHandler(res,404,"address not found")
-        }
-        return errorHandler(res,200,"address get sucessful",addresss)
-    }catch(err){
-        return errorHandler(res,500,`server error ${err.message}`)
+export const fetchAddress = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const addresss = await addressModel.find({ userId });
+    if (!addresss) {
+      return errorHandler(res, 404, "address not found");
     }
-}
+    return errorHandler(res, 200, "address get sucessful", addresss);
+  } catch (err) {
+    return errorHandler(res, 500, `server error ${err.message}`);
+  }
+};
 //! edit address
-export const editAddress=async (req,res)=>{
-    const {userId,addressId}=req.params;
-    const formData=req.body;
-    if(!userId || !addressId){
-        return errorHandler(res,400,"invaid data provided")
+export const editAddress = async (req, res) => {
+  const { userId, addressId } = req.params;
+  const formData = req.body;
+  if (!userId || !addressId) {
+    return errorHandler(res, 400, "invaid data provided");
+  }
+  try {
+    const address = await addressModel.findOneAndUpdate(
+      { _id: addressId, userId },
+      formData,
+      { new: true }
+    );
+    if (!address) {
+      return errorHandler(res, 404, "address not found");
     }
-    try{
-      const address=await addressModel.findOneAndUpdate({_id:addressId,userId},formData,{new:true})
-      if(!address){
+    return errorHandler(res, 200, "update sucessful", address);
+  } catch (err) {
+    return errorHandler(res, 500, `server error ${err.message}`);
+  }
+};
+//! delete address
+export const deleteAddress = async (req, res) => {
+  const { userId, addressId } = req.params;
+  if (!userId || !addressId) {
+    return errorHandler(res, 400, "enter valid data");
+  }
+  try {
+    const address = await addressModel.findOneAndDelete({
+      _id: addressId,
+      userId,
+    });
+    if(!address){
         return errorHandler(res,404,"address not found")
-      }
-      return errorHandler(res,200,"update sucessful",address)
-    }catch(err){
-        return errorHandler(res,500,`server error ${err.message}`)
     }
-}
+    return errorHandler(res,200,"address delete sucssful",address)
+  } catch (err) {
+    return errorHandler(res, 500, `server error ${err.message}`);
+  }
+};
--- backend/server/routes/address.route.js
+++ backend/server/routes/address.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createAddress, editAddress, fetchAddress } from '../controller/address.controller.js'
+import { createAddress, deleteAddress, editAddress, fetchAddress } from '../controller/address.controller.js'
 const addressRouter=express.Router()
 addressRouter.post("/create",createAddress)
 addressRouter.get("/get/:userId",fetchAddress)
 addressRouter.put("/update/:userId/:addressId",editAddress)
+addressRouter.delete("/delete/:userId/:addressId",deleteAddress)
 export default addressRouter;
