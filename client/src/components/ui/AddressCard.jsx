3f119a18c38e5b467d11e8e4e2a430ab55eb2ede
rahulprops <rahulhack912@gmail.com>
2025-02-08 00:41:16 +0530

    create order page design

---
+++ client/src/components/ui/AddressCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div className=' w-full shadow'>
+        <div className=' px-2'>
+            <div>Address:India</div>
+            <div>Address:India</div>
+            <div>Address:India</div>
+            <div>Address:India</div>
+        </div>
+    </div>
+  )
+}
+
+export default AddressCard
--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,318 +1,158 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import AddressCard from "../components/ui/AddressCard";
+import {
+  useDeleteCartMutation,
+  useGetCartQuery,
+  useUpdateCartMutation,
+} from "../features/apis/cartApi";
+import { FaMinus, FaPlus, FaTrash } from "react-icons/fa"; // Import react-icons
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
-  const [billingDetails, setBillingDetails] = useState({
-    fullName: '',
-    email: '',
-    phone: '',
-    address: '',
-    city: '',
-    state: '',
-    postalCode: '',
-  });
+  // Fetch cart data using the API
+  const { data: cart, isLoading, isSuccess, error } = useGetCartQuery();
+  const [updateCart, { isSuccess: cartUpdateSuccess }] =
+    useUpdateCartMutation();
+  const [deleteCart, { isSuccess: deleteCartSucess }] = useDeleteCartMutation();
 
-  const [shippingDetails, setShippingDetails] = useState({
-    fullName: '',
-    address: '',
-    city: '',
-    state: '',
-    postalCode: '',
-  });
+  // Handle increment of quantity
+  const incrementQuantity = (productId) => {
+    const quantity = 1;
+    updateCart({ productId, quantity });
+  };
 
-  const [paymentDetails, setPaymentDetails] = useState({
-    cardNumber: '',
-    cardName: '',
-    expirationDate: '',
-    cvv: '',
-  });
+  // Handle decrement of quantity
+  const decrementQuantity = (productId) => {
+    const quantity = -1;
+    updateCart({ productId, quantity });
+  };
 
-  const [orderSummary, setOrderSummary] = useState({
-    items: [
-      { id: 1, name: 'Product 1', price: 100, quantity: 1 },
-      { id: 2, name: 'Product 2', price: 200, quantity: 2 },
-    ],
-    shippingCost: 20,
-    tax: 30,
-    totalAmount: 0,
-  });
+  // Handle delete item from cart
+  const handleDeleteItem = (productId) => {
+    deleteCart(productId);
+  };
 
-  const handleInputChange = (e, section) => {
-    const { name, value } = e.target;
-    if (section === 'billing') {
-      setBillingDetails((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
-    } else if (section === 'shipping') {
-      setShippingDetails((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
-    } else if (section === 'payment') {
-      setPaymentDetails((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
+  useEffect(() => {
+    if (cartUpdateSuccess) {
+      alert("Cart updated successfully");
     }
+    if (deleteCartSucess) {
+      alert("Item deleted successfully");
+    }
+  }, [cartUpdateSuccess, deleteCartSucess]);
+
+  // Calculate total price
+  const calculateTotal = () => {
+    return cart
+      .reduce((total, item) => total + item.salePrice * item.quantity, 0)
+      .toFixed(2);
   };
 
-  const calculateTotalAmount = () => {
-    const itemsTotal = orderSummary.items.reduce(
-      (sum, item) => sum + item.price * item.quantity,
-      0
+  // Loading and error handling
+  if (isLoading)
+    return <div className="text-center py-10">Loading cart items...</div>;
+  if (error)
+    return (
+      <div className="text-center py-10 text-red-500">
+        Failed to load cart items.
+      </div>
     );
-    const total = itemsTotal + orderSummary.shippingCost + orderSummary.tax;
-    setOrderSummary((prevState) => ({
-      ...prevState,
-      totalAmount: total,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle payment processing logic here
-    console.log('Processing order...');
-  };
-
-  React.useEffect(() => {
-    calculateTotalAmount();
-  }, [orderSummary.items, orderSummary.shippingCost, orderSummary.tax]);
 
   return (
     <div className="container mx-auto p-6">
-      <div className="text-3xl font-bold mb-6">Checkout</div>
-
-      <form onSubmit={handleSubmit}>
-        {/* Billing Information */}
-        <div className="bg-white p-6 rounded-md shadow-md mb-6">
-          <h3 className="text-2xl font-semibold mb-4">Billing Information</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div>
-              <label className="block text-gray-700">Full Name</label>
-              <input
-                type="text"
-                name="fullName"
-                value={billingDetails.fullName}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={billingDetails.email}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Phone</label>
-              <input
-                type="text"
-                name="phone"
-                value={billingDetails.phone}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Address</label>
-              <input
-                type="text"
-                name="address"
-                value={billingDetails.address}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">City</label>
-              <input
-                type="text"
-                name="city"
-                value={billingDetails.city}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">State</label>
-              <input
-                type="text"
-                name="state"
-                value={billingDetails.state}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Postal Code</label>
-              <input
-                type="text"
-                name="postalCode"
-                value={billingDetails.postalCode}
-                onChange={(e) => handleInputChange(e, 'billing')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
+      <div className="grid gap-6 grid-cols-2">
+        {/* Address Section */}
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Shipping Address</h2>
+          <div className="grid gap-4 grid-cols-2">
+            <AddressCard />
+            <AddressCard />
           </div>
         </div>
 
-        {/* Shipping Information */}
-        <div className="bg-white p-6 rounded-md shadow-md mb-6">
-          <h3 className="text-2xl font-semibold mb-4">Shipping Information</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div>
-              <label className="block text-gray-700">Full Name</label>
-              <input
-                type="text"
-                name="fullName"
-                value={shippingDetails.fullName}
-                onChange={(e) => handleInputChange(e, 'shipping')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
+        {/* Cart Section */}
+        <div>
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-2xl font-semibold mb-6">Your Cart</h2>
+
+            {/* Cart Items */}
+            <div className="space-y-6">
+              {isSuccess &&
+                cart.map((item) => (
+                  <div
+                    key={item._id}
+                    className="flex items-center justify-between bg-white p-4 rounded-lg shadow-md"
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="w-24 h-24 object-cover rounded-md"
+                    />
+                    <div className="flex-1 ml-4">
+                      <h3 className="text-lg font-semibold text-gray-800">
+                        {item.title}
+                      </h3>
+                      {/* Sale price if available */}
+                      <p className="text-gray-600">
+                        ${item.salePrice.toFixed(2)}{" "}
+                        {item.salePrice < item.price && (
+                          <span className="text-sm text-gray-500 line-through ml-2">
+                            ${item.price}
+                          </span>
+                        )}
+                      </p>
+                    </div>
+
+                    {/* Quantity Control */}
+                    <div className="flex items-center space-x-2">
+                      <button
+                        onClick={() => decrementQuantity(item.productId)}
+                        className="px-3 py-1 bg-gray-300 rounded-full text-gray-700 hover:bg-gray-400"
+                      >
+                        <FaMinus />
+                      </button>
+                      <span className="text-lg font-semibold">
+                        {item.quantity}
+                      </span>
+                      <button
+                        onClick={() => incrementQuantity(item.productId)}
+                        className="px-3 py-1 bg-gray-300 rounded-full text-gray-700 hover:bg-gray-400"
+                      >
+                        <FaPlus />
+                      </button>
+                    </div>
+
+                    {/* Item Total Price */}
+                    <div className="ml-4 text-gray-800 font-semibold">
+                      ${(item.salePrice * item.quantity).toFixed(2)}
+                    </div>
+
+                    {/* Delete Button */}
+                    <button
+                      onClick={() => handleDeleteItem(item.productId)}
+                      className="text-red-600 px-2 hover:text-red-800"
+                    >
+                      <FaTrash />
+                    </button>
+                  </div>
+                ))}
             </div>
-            <div>
-              <label className="block text-gray-700">Address</label>
-              <input
-                type="text"
-                name="address"
-                value={shippingDetails.address}
-                onChange={(e) => handleInputChange(e, 'shipping')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
+
+            {/* Cart Total Price */}
+            <div className="flex justify-between items-center mt-6 p-4 bg-gray-100 rounded-lg shadow-md">
+              <span className="text-lg font-semibold">Total Price:</span>
+              <span className="text-xl font-bold">${calculateTotal()}</span>
             </div>
-            <div>
-              <label className="block text-gray-700">City</label>
-              <input
-                type="text"
-                name="city"
-                value={shippingDetails.city}
-                onChange={(e) => handleInputChange(e, 'shipping')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">State</label>
-              <input
-                type="text"
-                name="state"
-                value={shippingDetails.state}
-                onChange={(e) => handleInputChange(e, 'shipping')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Postal Code</label>
-              <input
-                type="text"
-                name="postalCode"
-                value={shippingDetails.postalCode}
-                onChange={(e) => handleInputChange(e, 'shipping')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
+
+            {/* Checkout Button */}
+            <div className="mt-6">
+              <button className="w-full py-3 px-10 bg-blue-600 cursor-pointer text-white rounded-lg hover:bg-blue-700">
+                create order
+              </button>
             </div>
           </div>
         </div>
-
-        {/* Payment Information */}
-        <div className="bg-white p-6 rounded-md shadow-md mb-6">
-          <h3 className="text-2xl font-semibold mb-4">Payment Information</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div>
-              <label className="block text-gray-700">Card Number</label>
-              <input
-                type="text"
-                name="cardNumber"
-                value={paymentDetails.cardNumber}
-                onChange={(e) => handleInputChange(e, 'payment')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Cardholder Name</label>
-              <input
-                type="text"
-                name="cardName"
-                value={paymentDetails.cardName}
-                onChange={(e) => handleInputChange(e, 'payment')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">Expiration Date</label>
-              <input
-                type="text"
-                name="expirationDate"
-                value={paymentDetails.expirationDate}
-                onChange={(e) => handleInputChange(e, 'payment')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700">CVV</label>
-              <input
-                type="text"
-                name="cvv"
-                value={paymentDetails.cvv}
-                onChange={(e) => handleInputChange(e, 'payment')}
-                className="mt-2 px-4 py-2 w-full border border-gray-300 rounded-md"
-                required
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* Order Summary */}
-        <div className="bg-white p-6 rounded-md shadow-md mb-6">
-          <h3 className="text-2xl font-semibold mb-4">Order Summary</h3>
-          <div className="space-y-4">
-            {orderSummary.items.map((item) => (
-              <div key={item.id} className="flex justify-between">
-                <span>{item.name} (x{item.quantity})</span>
-                <span>${item.price * item.quantity}</span>
-              </div>
-            ))}
-            <div className="flex justify-between font-semibold">
-              <span>Shipping</span>
-              <span>${orderSummary.shippingCost}</span>
-            </div>
-            <div className="flex justify-between font-semibold">
-              <span>Tax</span>
-              <span>${orderSummary.tax}</span>
-            </div>
-            <div className="flex justify-between font-bold">
-              <span>Total</span>
-              <span>${orderSummary.totalAmount}</span>
-            </div>
-          </div>
-        </div>
-
-        {/* Place Order Button */}
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
-        >
-          Place Order
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
