ab9681f7b2249931354e2da71d33b0e991f8f5ed
rahulprops <rahulhack912@gmail.com>
2025-02-07 23:58:14 +0530

    order model create

---
+++ backend/server/model/order.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const OrderSchema=new mongoose.Schema({
+    userId:String,
+    cartItems:[
+        {
+            productId:String,
+            title:String,
+            Image:String,
+            price:String,
+            salePrice:String
+        }
+    ],
+    address:{
+        addressId:String,
+        address:String,
+        city:String,
+        pincode:String,
+        phone:String,
+        notes:String,
+    },
+    orderStatus:String,
+    paymentMethod:String,
+    paymentStauts:String,
+    totalAmonut:Number,
+    orderData:Date,
+},{timestamps:true})
+
+ const orderModel=mongoose.model("order",OrderSchema)
+ export default orderModel;
